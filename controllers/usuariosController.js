896a2646005bd8426f0164e483a64691e494f8a9
ThomeDvlp <thomedvlt@gmail.com>
2021-07-02 15:20:24 -0300

    Correção Rota Get

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -2,19 +2,9 @@ const Usuario = require('../models/usuario')
 const con = require('../infraestrutura/connection')
 
 module.exports = app=> {
-    app.get('/cadastro', function getTarefas() {
-      return(req, res)=>{
-        con.query(`SELECT * FROM usuarios`, usuario, (error, results)=>{
-          if(error){
-            res.status(400).json(error);
-          } else {
-            res.status(201).json(results);
-          }
-        })
-      }
-    // (req, res)=>{
+    app.get('/cadastro', (req, res)=>{
 
-    //   res.send('Servidor rodando, tuto OK')
+      res.send('Servidor rodando, tuto OK')
   })
 
     app.post('/cadastro', (req, res)=> {
